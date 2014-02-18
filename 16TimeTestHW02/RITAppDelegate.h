4adff1fb9a78aa4be871bce4d76a124347a0e00b
Pronin Alexander <pronin.alx@gmail.com>
2014-02-18 16:07:05 +0200

    Initial Commit

---
+++ 16TimeTestHW02/RITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RITAppDelegate.h
+//  16TimeTestHW02
+//
+//  Created by Aleksandr Pronin on 18.02.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
