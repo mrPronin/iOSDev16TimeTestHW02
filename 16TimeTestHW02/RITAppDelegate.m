002c468cf0ba6c7b5654a46f872c04b2f2e4886e
Alexander Pronin <pronin.alx@gmail.com>
2014-02-19 02:15:03 +0200

    Final commit

--- 16TimeTestHW02/RITAppDelegate.m
+++ 16TimeTestHW02/RITAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "RITAppDelegate.h"
+#import "RITStudent.h"
 
 @implementation RITAppDelegate
 
@@ -16,6 +17,132 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    NSInteger           studentsNumber  = 30;
+    NSMutableArray*     students        = [NSMutableArray array];
+    NSCalendar*         calendar        = [NSCalendar currentCalendar];
+    NSArray*            firstNames      = @[@"Lex", @"Conrad", @"Alan", @"Alex", @"Amos", @"Bart", @"Ben", @"Bill", @"Bob", @"Jim"];
+    NSArray*            lastNames       = @[@"Smith", @"Johnson", @"Brown", @"Brown", @"Moore", @"Robinson", @"King", @"Scott", @"Evans", @"Bell"];
+    NSInteger           SecondsInDay    = 60 * 60 * 24;
+    
+    /*
+    // How many days until the end of the year?
+    // How many days before the beginning of the year?
+    
+    // Number of day in the year
+    NSDate*             today           = [NSDate date];
+    NSInteger           dayOfYear       = [calendar
+                                           ordinalityOfUnit:NSCalendarUnitDay
+                                           inUnit:NSCalendarUnitYear
+                                           forDate:today];
+    NSLog(@"Number of today in the year: %d", dayOfYear);
+    
+    // How many days in a year?
+    NSDateComponents*   lastDayComp     = [[NSDateComponents alloc] init];
+    lastDayComp.year                    = 2014;
+    lastDayComp.month                   = 12;
+    lastDayComp.day                     = 31;
+    NSDate*             lastDay         = [calendar dateFromComponents:lastDayComp];
+    NSInteger           daysNumber      = [calendar
+                                           ordinalityOfUnit:NSCalendarUnitDay
+                                           inUnit:NSCalendarUnitYear
+                                           forDate:lastDay];
+    NSLog(@"Number of days in the year: %d", daysNumber);
+    
+    // Days number in this month
+    NSRange rangeDaysInMonth            = [calendar rangeOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitMonth forDate:today];
+    NSLog(@"Days number in this month: %d", rangeDaysInMonth.length);
+    
+    // Days number in this year
+    NSRange rangeDaysInYear             = [calendar rangeOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitYear forDate:today];
+    NSLog(@"Days number in this month: %d", rangeDaysInYear.length);
+    NSDate* date                    = [calendar
+                                       dateByAddingComponents:dateCompontnts
+                                       toDate:dateNow
+                                       options:0];
+    
+     */
+    
+    for (int i = 0; i < studentsNumber; i++) {
+        
+        RITStudent*     student         = [RITStudent studentWithlastName:lastNames[arc4random() % 10] andFirstName:firstNames[arc4random() % 10]];
+        student.personalID              = i + 1;
+        
+        // define first date for year of birth
+        NSInteger           studentAge  = arc4random() % 35 + 16;
+        NSDate*             today       = [NSDate date];
+        NSDateComponents*   comp        = [calendar components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay fromDate:today];
+        comp.year   -= studentAge;
+        comp.month  = 1;
+        comp.day    = 1;
+        NSDate* firstDateOfYear         = [calendar dateFromComponents:comp];
+        
+        comp.month  = 12;
+        comp.day    = 31;
+        NSDate* lastDateOfYear          = [calendar dateFromComponents:comp];
+        
+        // days number in the birth year
+        NSInteger           daysNumber  = [calendar
+                                               ordinalityOfUnit:NSCalendarUnitDay
+                                               inUnit:NSCalendarUnitYear
+                                               forDate:lastDateOfYear];
+        
+        student.dateOFBirth             = [NSDate dateWithTimeInterval:arc4random() % (daysNumber + 1) * SecondsInDay sinceDate:firstDateOfYear];
+        
+        [students addObject:student];
+    }
+    
+    NSComparisonResult (^sortingBlock)(id, id) = ^(id obj01, id obj02) {
+        
+        RITStudent* student01   = (RITStudent*)obj01;
+        RITStudent* student02   = (RITStudent*)obj02;
+        
+        return [[student02 dateOFBirth] compare:[student01 dateOFBirth]];
+        
+    };
+    
+    NSArray*    sortedByDate  = [students sortedArrayUsingComparator:sortingBlock];
+    
+    NSLog(@"Students sorted by bitrh date:");
+    for (RITStudent* student in sortedByDate) {
+        NSLog(@"%@", student);
+    }
+
+    NSComparisonResult (^sortingBlockName)(id, id) = ^(id obj01, id obj02) {
+        
+        NSString*   firstName01     = [obj01 firstName];
+        NSString*   firstName02     = [obj02 firstName];
+        NSString*   lastName01      = [obj01 lastName];
+        NSString*   lastName02      = [obj02 lastName];
+        NSDate*     birthDate01     = [obj01 dateOFBirth];
+        NSDate*     birthDate02     = [obj02 dateOFBirth];
+        
+        if ([firstName01 isEqualToString:firstName02]) {
+            
+            if ([lastName01 isEqualToString:lastName02]) {
+                if ([birthDate01 isEqualToDate:birthDate02]) {
+                    return NSOrderedSame;
+                } else {
+                    return [birthDate02 compare:birthDate01];
+                }
+            } else {
+                return [lastName01 compare:lastName02];
+            }
+            
+        } else {
+            return [firstName01 compare:firstName02];
+        }
+        
+        return NSOrderedAscending;
+    };
+    
+    NSArray*    sortedByName        = [students sortedArrayUsingComparator:sortingBlockName];
+    NSLog(@"\n");
+    NSLog(@"Students sorted by first name, last name and date:");
+    for (RITStudent* student in sortedByName) {
+        NSLog(@"%@", student);
+    }
+    
     return YES;
 }
 
